572a807a422eaeb4bbb1e62b02bc56c06d7479cb
Asfand-Yar-Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2023-09-19 23:54:50 -0400

    Add a spinner and enable scrolling

--- src/screens/GenerateOutfitScreen.js
+++ src/screens/GenerateOutfitScreen.js
@@ -1,89 +1,142 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, TextInput, StyleSheet, Image } from 'react-native';
-import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
-import * as SecureStore from 'expo-secure-store';
+import React, { useState, useEffect } from 'react'
+import {
+  ActivityIndicator,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Image,
+} from 'react-native'
+import axios from 'axios'
+import { useNavigation } from '@react-navigation/native'
+import * as SecureStore from 'expo-secure-store'
 
 const GenerateOutfitScreen = () => {
-  const [description, setDescription] = useState('');
-  const [generatedOutfit, setGeneratedOutfit] = useState(null);
-  const [topImage, setTopImage] = useState(null);
-  const [bottomImage, setBottomImage] = useState(null);
+  const [description, setDescription] = useState('')
+  const [generatedOutfit, setGeneratedOutfit] = useState(null)
+  const [topImage, setTopImage] = useState(null)
+  const [bottomImage, setBottomImage] = useState(null)
+  const [loading, setLoading] = useState(false)
 
-  const navigation = useNavigation();
+  const navigation = useNavigation()
 
   const handleGenerateOutfit = async () => {
-    const accessToken = await SecureStore.getItemAsync('accessToken');
+    const accessToken = await SecureStore.getItemAsync('accessToken')
     try {
-      let response;
+      setLoading(true)
+      let response
       if (description) {
-        const formData = new FormData();
-        formData.append('desc' , description);
+        const formData = new FormData()
+        formData.append('desc', description)
 
-        response = await axios.post('https://vcloset.xyz/api/generateOutfitsWithDescription', formData, {
+        response = await axios.post(
+          'https://vcloset.xyz/api/generateOutfitsWithDescription',
+          formData,
+          {
             headers: {
-                'Authorization': `Bearer ${accessToken}`,
+              Authorization: `Bearer ${accessToken}`,
             },
-        });
+          }
+        )
+        setLoading(false)
       } else {
         response = await axios.get('https://vcloset.xyz/api/generateOutfits', {
           headers: {
-            'Authorization': `Bearer ${accessToken}`,
+            Authorization: `Bearer ${accessToken}`,
           },
-        });
+        })
+        setLoading(false)
       }
-      console.log('Generated outfit:', response.data);
-      setGeneratedOutfit(response.data);
-      const top = parseInt(response.data.top);
+      console.log('Generated outfit:', response.data)
+      setGeneratedOutfit(response.data)
+      const top = parseInt(response.data.top)
       // Fetch and set top image
-      const topResponse = await axios.get(`https://vcloset.xyz/api/items/${top}`, {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-            },
-        });
-      setTopImage(topResponse.data.image.blob);
+      const topResponse = await axios.get(
+        `https://vcloset.xyz/api/items/${top}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      setTopImage(topResponse.data.image.blob)
 
-      const bottom = parseInt(response.data.bottom);
+      const bottom = parseInt(response.data.bottom)
       // Fetch and set bottom image
-      const bottomResponse = await axios.get(`https://vcloset.xyz/api/items/${bottom}`, {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-            },
-        });
-      setBottomImage(bottomResponse.data.image.blob);
+      const bottomResponse = await axios.get(
+        `https://vcloset.xyz/api/items/${bottom}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      setBottomImage(bottomResponse.data.image.blob)
+      setLoading(false)
     } catch (error) {
-      console.error('Error generating outfit:', error.response.data);
+      console.error('Error generating outfit:', error.response.data)
     }
-  };
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Generate Outfit</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title} alwaysBounceVertical={false}>
+        Generate Outfit
+      </Text>
       <TextInput
         style={styles.descriptionInput}
-        placeholder="Enter outfit description (optional)"
+        placeholder='Enter outfit description (optional)'
         value={description}
         onChangeText={(text) => setDescription(text)}
       />
-      <TouchableOpacity style={styles.generateButton} onPress={handleGenerateOutfit}>
+      <TouchableOpacity
+        style={styles.generateButton}
+        onPress={handleGenerateOutfit}>
         <Text style={styles.buttonText}>Generate</Text>
       </TouchableOpacity>
-      {generatedOutfit && (
-        <View style={styles.generatedOutfit}>
-          <Text style={styles.outfitText}>Top: {generatedOutfit.top}</Text>
-          {topImage && <Image source={{ uri: `data:image/png;base64,${topImage}` }} style={styles.outfitImage} />}
-          <Text style={styles.outfitText}>Bottom: {generatedOutfit.bottom}</Text>
-          {bottomImage && <Image source={{ uri: `data:image/png;base64,${bottomImage}` }} style={styles.outfitImage} />}
-          <Text style={styles.outfitDescription}>{generatedOutfit.description}</Text>
+      {loading && (
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator />
         </View>
       )}
-    </View>
-  );
-};
+      {generatedOutfit && (
+        <>
+          <View style={styles.generatedOutfit}>
+            <Text style={styles.outfitText}>Top: {generatedOutfit.top}</Text>
+            {topImage && (
+              <Image
+                source={{ uri: `data:image/png;base64,${topImage}` }}
+                style={styles.outfitImage}
+              />
+            )}
+            <Text style={styles.outfitText}>
+              Bottom: {generatedOutfit.bottom}
+            </Text>
+            {bottomImage && (
+              <Image
+                source={{ uri: `data:image/png;base64,${bottomImage}` }}
+                style={styles.outfitImage}
+              />
+            )}
+            <Text style={styles.outfitDescription}>
+              {generatedOutfit.description}
+            </Text>
+          </View>
+          <TouchableOpacity style={styles.saveButton}>
+            <Text style={styles.buttonText}>Save Outfit</Text>
+          </TouchableOpacity>
+        </>
+      )}
+    </ScrollView>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
     paddingHorizontal: 20,
@@ -108,6 +161,13 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
   },
+  saveButton: {
+    backgroundColor: '#007BFF',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
   buttonText: {
     color: 'white',
     fontSize: 16,
@@ -131,6 +191,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 16,
   },
-});
+})
 
-export default GenerateOutfitScreen;
+export default GenerateOutfitScreen
